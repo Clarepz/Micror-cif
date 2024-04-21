ae5820ecef5788d98baa771ac61f815e3c6b6e69
Clarepz <clarence.provenaz@gmail.com>
2024-04-21 12:56:26 +0200

    removed save as in projet.cc

--- projet.cc
+++ projet.cc
@@ -12,7 +12,6 @@ int main(int argc, char* argv[]){
 
     if(argc != 2) exit(EXIT_FAILURE);
     Simulation simulation(argv[1]); //initialise une simulation a partir du fichier d'entree
-    simulation.saveAs("save.txt");
 
     MyEvent::setSimulation(simulation);
 
